6bac6fd565a96541254f06aff1f3471874299fcd
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-03-23 21:40:39 -0300

    feat(estilos): criando estilo global com styled-components

---
+++ src/styles/global.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+html {
+    @media (max-width: 1200px) {
+        font-size: 93.75%;
+    }
+    @media (max-width: 768px) {
+        font-size: 87.5%;
+    }     
+}
+
+body {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.6;
+}
+
+textarea, input, button, strong {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400
+}
+
+h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+}
+
+button {
+    cursor: pointer;
+}
+
+[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+}
+
+` 
