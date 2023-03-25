ddbdec6d244a4c329b2c8b3d2ca959951cc23644
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-03-24 23:48:46 -0300

    feat(header): criando o header e renderizando em tela

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,15 @@
+import { MapPinLine, ShoppingCartSimple } from 'phosphor-react';
+import coffeeLogo from '../../assets/coffeeLogo.svg'
+import { HeaderContainer } from './style';
+
 export function Header() {
     return (
-        <header> HEADER </header>
+        <HeaderContainer> 
+            <img src={coffeeLogo} alt="Logo do site" />
+            <nav>
+                <span> <MapPinLine size={24} weight="fill" /> Porto Alegre, RS </span>
+                <a href="#"> <ShoppingCartSimple size={24} weight="fill" /></a>
+            </nav>
+        </HeaderContainer>
     );
 }
---
+++ src/components/Header/style.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+
+padding: 2rem 10rem;
+display: flex;
+justify-content: space-between;
+align-items: center;
+margin: 0 auto;
+
+nav {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    span {
+        background-color: ${(props) => props.theme['purple-light']};
+        color: ${props => props.theme['purple-dark']};
+        padding: 0.5rem 1rem;
+        border-radius: 4px;
+
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+    }
+    a {
+        background-color: ${(props) => props.theme['yellow-light']};
+        color: ${props => props.theme['yellow-dark']};
+        padding: 0.5rem;
+        border-radius: 4px;
+
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+    }
+}
+`
