a8a44e53eea954c66244af7347edf7f8ba86c887
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-03-24 23:06:34 -0300

    feat(router): configurando router
    Router para navegação das páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { GlobalStyles } from "./styles/global";
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from "./Router";
 
 export function App() {
 
   return (
-    <>
-      <h1> Hello World </h1>
+    <BrowserRouter>
+      <Router />
       <GlobalStyles />
-    </>
+    </BrowserRouter>
   )
 }
 
