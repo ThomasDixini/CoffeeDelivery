2f69a37890c7a6e2fdb52cde8ce31003ec09df66
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-04-02 23:29:10 -0300

    feat(html): criando HTML
    Estruturando a parte de HTML da páginca Checkout

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,9 +1,43 @@
-import { CheckoutMain } from "./styles";
+import { AdressContainer, CartContainer, CheckoutMain } from "./styles";
 
 export function Checkout() {
     return (
         <CheckoutMain>
-            
+            <AdressContainer>
+                <h1> Complete seu pedido </h1>
+                <div>
+                    <header>
+
+                    </header>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <input type="number"/>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <header>
+
+                    </header>
+                    <button type="button"> a </button>
+                    <button type="button"> a </button>
+                    <button type="button"> a </button>
+                </div>
+            </AdressContainer>
+            <CartContainer>
+                <h1> Cafés Selecionados </h1>
+                <div>
+                    <article>
+
+                    </article>
+                    <span></span>
+                    <span></span>
+                    <strong></strong>
+                    <button type="submit"></button>
+                </div>
+            </CartContainer>
         </CheckoutMain>
     );
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -3,3 +3,9 @@ import styled from "styled-components";
 export const CheckoutMain = styled.main`
 
 `
+export const AdressContainer = styled.section`
+
+`
+export const CartContainer = styled.section`
+
+`
